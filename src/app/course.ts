5307f1873684abd0baa3e745dc18c733a90a9776
Gabriel Lima <gcamilo7@gmail.com>
2018-10-10 00:22:23 +0900

    added topic template and topic course classes

---
+++ src/app/course.ts
@@ -0,0 +1,13 @@
+import { Topic } from './topic';
+
+export class Course {
+  id: number;
+  //creator: Professor;
+  //contributors: Profesor[];
+  //discussions: Discussion[];
+  topics: Topic[];
+  date: Date;
+  //universities: University[];
+  //graph: No idea;
+
+}
---
+++ src/app/topic.ts
@@ -0,0 +1,11 @@
+export class Topic {
+  id: number;
+  title: string;
+  description: string;
+  //creator: Professor;
+  //contributors: Professor[];
+  tags: string[];
+  //materials: Material[];
+  //universities: University[];
+  date: Date;
+}
