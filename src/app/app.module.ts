71f9e5c09f09f6d3edb91517ab7770ab02d54339
Gabriel Lima <gcamilo7@gmail.com>
2018-10-13 21:41:09 +0900

    design start

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DiscussionComponent } from './discussion/discussion.component';
+import { SearchComponent } from './search/search.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DiscussionComponent,
+    SearchComponent
   ],
   imports: [
     BrowserModule,
