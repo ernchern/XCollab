e22e3ee639d250b6d3defd61543b2a5d75d50332
Oguz Serbetci <oguz.serbetci@gmail.com>
2018-11-15 23:54:00 +0900

    Show mastery in user profile, make navbar more beautiful.

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -13,16 +13,52 @@ import { Chart } from 'chart.js'
 })
 export class UserComponent implements OnInit {
   user: User;
+  mastery: number[];
+  masteryLabels: string[];
+  masteries: number[];
 
   constructor(private exerciseService: ExerciseService,
     private route: ActivatedRoute,
     private location: Location
-  ) { }
+  ) {}
 
   getData() {
     const user_id = this.route.snapshot.paramMap.get('user_id');
     console.log(user_id)
-    this.exerciseService.getUser(user_id).subscribe(u => this.user = u[0]);
+    this.exerciseService.getUser(user_id).subscribe(u => {this.user = u[0];
+                                                          this.mastery = this.getMasteryTotal();
+                                                          console.log(this.mastery)
+                                                          this.setupChart();
+                                                          this.getMasteries()});
+  }
+
+  getMasteries() {
+    this.masteryLabels = this.user.mastery.map(m => m.tag)
+    this.masteries = this.user.mastery.map(m => m.actions.length)
+    console.log(this.masteryLabels, this.masteries)
+  }
+
+  getMasteryTotal(): number[] {
+    var discussions = 0;
+    var comments = 0;
+    var solutions = 0;
+    for (var mastery of this.user.mastery) {
+      for (var action of mastery.actions) {
+        switch(action) {
+          case "d":
+            discussions += 1;
+            break;
+          case "c":
+            comments += 1;
+            break;
+          case "s":
+            solutions += 1;
+          default:
+            break;
+        }
+      }
+    }
+    return [comments, solutions, discussions]
   }
 
   setupChart() {
@@ -33,7 +69,7 @@ export class UserComponent implements OnInit {
         labels: ['Talker', 'Solver', 'Questioner'],
         datasets: [{
           label: "you",
-          data: [15, 10, 5],
+          data: this.mastery,
           backgroundColor: [
             'rgba(255, 99, 132, 0.2)',
           ],
