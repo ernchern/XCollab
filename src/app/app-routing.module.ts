676150c830437a63eec42d9cd0556a416bf00d0e
Gabriel Lima <gcamilo7@gmail.com>
2018-11-19 18:44:16 +0900

    unfixed routing because fix didnt work

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,7 +6,7 @@ import { DiscussionComponent } from './discussion/discussion.component';
 import { UserComponent } from './user/user.component';
 
 const routes: Routes = [
-  { path: '**', redirectTo: 'search', pathMatch: 'full' },
+  { path: '', redirectTo: 'search', pathMatch: 'full' },
   { path: 'search', component: SearchComponent },
   { path: 'exercise/:exercise_id', component: ExerciseComponent},
   { path: 'exercise/:exercise_id/discussion/:discussion_id', component: DiscussionComponent},
